63bdcbf73244b29450ac86f820699d9efd7a8158
Wael Al Jishi <wa910@doc.ic.ac.uk>
2013-02-21 00:08:53 +0000

    Added script to build database

---
+++ db/build-tables.sql
@@ -0,0 +1,19 @@
+-- USAGE: In order to create the database run the following command.
+--        Note that this does not work if a file already exists.
+--
+--        sqlite3 example.db < build-tables.sql
+CREATE TABLE experiment (
+     experimentid serial unique Primary Key,
+     sha CHARACTER(40),
+     description text
+   );
+
+CREATE TABLE data (
+     dataid serial unique Primary Key,
+     experimentid integer References experiment,
+     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     stdout text,
+     stderr text
+     -- TODO Add auto-computed columns in the future as fit
+     -- e.g. num cpu cores, min quality, exec time, etc.
+   );
