5b1e5dff9155879c25303585d8c55c84f6b15335
Wael Al Jishi <wa910@doc.ic.ac.uk>
2013-03-02 20:49:19 +0000

    create-experiment.py can now take variable args (for 'description')

--- db/create-experiment.py
+++ db/create-experiment.py
@@ -9,7 +9,7 @@ DB_FILE = 'experiments.db'
 parser = argparse.ArgumentParser(description='Create an experiment in the database. If successful, writes the experiment id to stdout.')
 parser.add_argument('sha', metavar='SHA', type=str,
                    help='The SHA of the associated git commit')
-parser.add_argument('desc', metavar='DESC', type=str,
+parser.add_argument('desc', metavar='DESC', type=str, nargs=argparse.REMAINDER,
                    help='A description of the experiment')
 
 args = parser.parse_args()
@@ -17,5 +17,5 @@ args = parser.parse_args()
 
 # perform DB insertion
 with sqlite3.connect(DB_FILE) as conn:
-	cur = conn.execute('insert into experiment (sha, description) values (?,?)', (args.sha, args.desc))
+	cur = conn.execute('insert into experiment (sha, description) values (?,?)', (args.sha, ' '.join(args.desc)))
 	print cur.lastrowid
